55deaf4ad1d19bb53a547a4cd61b0b5b1bc04838
Andrew Udvare <audvare@gmail.com>
2025-05-20 20:41:50 -0400

    Translation infrastructure

---
+++ i18n/bpmdetect_en.ts
@@ -0,0 +1,130 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en">
+<context>
+    <name>DlgBPMDetect</name>
+    <message>
+        <location filename="../src/dlgbpmdetect.cpp" line="58"/>
+        <source>Add files</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/dlgbpmdetect.cpp" line="59"/>
+        <location filename="../src/dlgbpmdetect.cpp" line="331"/>
+        <source>Add directory</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/dlgbpmdetect.cpp" line="61"/>
+        <source>Remove selected tracks</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/dlgbpmdetect.cpp" line="62"/>
+        <source>Remove tracks with BPM</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/dlgbpmdetect.cpp" line="63"/>
+        <source>Clear list</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/dlgbpmdetect.cpp" line="66"/>
+        <source>Test BPM</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/dlgbpmdetect.cpp" line="70"/>
+        <source>Save BPM</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/dlgbpmdetect.cpp" line="71"/>
+        <location filename="../src/dlgbpmdetect.cpp" line="476"/>
+        <source>Clear BPM</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/dlgbpmdetect.cpp" line="78"/>
+        <source>BPM</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/dlgbpmdetect.cpp" line="78"/>
+        <source>Artist</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/dlgbpmdetect.cpp" line="78"/>
+        <source>Title</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/dlgbpmdetect.cpp" line="78"/>
+        <source>Length</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/dlgbpmdetect.cpp" line="78"/>
+        <source>Progress</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/dlgbpmdetect.cpp" line="79"/>
+        <source>Filename</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/dlgbpmdetect.cpp" line="169"/>
+        <source>Start</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/dlgbpmdetect.cpp" line="175"/>
+        <source>Stop</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/dlgbpmdetect.cpp" line="300"/>
+        <source>Adding %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/dlgbpmdetect.cpp" line="322"/>
+        <source>Add tracks</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/dlgbpmdetect.cpp" line="324"/>
+        <source>Audio files (*.wav *.mp3 *.ogg *.flac);;All files (*.*)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/dlgbpmdetect.cpp" line="409"/>
+        <source>Automatic BPM (beats per minute) detecting application.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/dlgbpmdetect.cpp" line="410"/>
+        <source> Version:    	%1 
+ Description:	%2 
+ License:    	GNU General Public License 
+ 
+ Author:     	Martin Sakmar 
+ e-mail:     	martin.sakmar@gmail.com 
+ </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/dlgbpmdetect.cpp" line="420"/>
+        <source>About BPM Detect</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/dlgbpmdetect.cpp" line="477"/>
+        <source>Clear BPMs of all selected tracks?</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
