83e5dca4c332e0c3cafa21bfc92ba6da4e276ca5
Andrew Udvare <audvare@gmail.com>
2025-08-09 04:50:49 -0400

    qdroplistview: mark overrides

--- src/widgets/qdroplistview.h
+++ src/widgets/qdroplistview.h
@@ -32,20 +32,20 @@
 class QDropListView : public QTreeWidget {
     Q_OBJECT
 public:
-    QDropListView(QWidget *parent = 0);
-    ~QDropListView();
+    QDropListView(QWidget *parent = nullptr);
+    ~QDropListView() override;
 
 public Q_SLOTS:
     /// Remove selected items from list
     void slotRemoveSelected();
 
 protected:
-    void keyPressEvent(QKeyEvent *e);
-    void keyReleaseEvent(QKeyEvent *e);
-    void dragEnterEvent(QDragEnterEvent *e);
-    void dragMoveEvent(QDragMoveEvent *e);
-    void dragLeaveEvent(QDragLeaveEvent *e);
-    void dropEvent(QDropEvent *e);
+    void keyPressEvent(QKeyEvent *e) override;
+    void keyReleaseEvent(QKeyEvent *e) override;
+    void dragEnterEvent(QDragEnterEvent *e) override;
+    void dragMoveEvent(QDragMoveEvent *e) override;
+    void dragLeaveEvent(QDragLeaveEvent *e) override;
+    void dropEvent(QDropEvent *e) override;
 
     Q_SIGNAL void keyPress(QKeyEvent *e);
     Q_SIGNAL void keyRelease(QKeyEvent *e);
