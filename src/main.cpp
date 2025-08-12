4cf1c9f8800bd7a297c5f42014965c2587c93501
Andrew Udvare <audvare@gmail.com>
2025-08-12 16:03:14 -0400

    main: switch to QCommandLine

--- src/main.cpp
+++ src/main.cpp
@@ -1,165 +1,119 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
-#include <cstdlib>
-#include <iostream>
-#include <string>
-
-#include <getopt.h>
+#include <QCommandLineParser>
+#ifndef NO_GUI
+#include <QApplication>
+#else
+#include <QCoreApplication>
+#endif
 
 #include "track/trackproxy.h"
 
 #ifndef NO_GUI
-#include <QApplication>
-
 #include "widgets/dlgbpmdetect.h"
 #endif
 
-using namespace std;
-
-const char *version = "0.7.2"; // App version
-
-void display_help() {
-    printf("BPM Detect version %s\n\n", version);
-    printf("Usage:\n bpmdetect [switches] [files]\n\n");
-    printf("Switches:\n");
-#ifndef NO_GUI
-    printf("-c --console         - run in console mode\n");
-#endif
-    printf("-h --help            - show this help\n"
-           "-s --save            - save BPMs to tags\n"
-           "-d --detect          - redetect (do not print BPMs stored in tags)\n"
-           "-r --remove          - remove stored BPMs from tags\n"
-           "-p --noprogress      - disable progress display (console)\n"
-           "-n --min <value>     - minimum BPM (default 80)\n"
-           "-x --max <value>     - maximum BPM (default 185)\n"
-           "-l --limit           - do not return BPM above the range <min, max>\n"
-           "-f --format <format> - set BPM format (default is 0.00)\n"
-           "\n");
-}
-
 int main(int argc, char *argv[]) {
-    bool console = false, redetect = false, bpmsave = false, clear = false, bformat = false,
-         progress = true;
-    QString format;
-
-    static struct option long_options[] = {
+    bool isConsoleMode = true;
 #ifndef NO_GUI
-        {"console", no_argument, nullptr, 'c'},
-#endif
-        {"format", required_argument, nullptr, 'f'},
-        {"save", no_argument, nullptr, 's'},
-        {"detect", no_argument, nullptr, 'd'},
-        {"remove", no_argument, nullptr, 'r'},
-        {"noprogress", no_argument, nullptr, 'p'},
-        {"min", required_argument, nullptr, 'n'},
-        {"max", required_argument, nullptr, 'x'},
-        {"limit", no_argument, nullptr, 'l'},
-        {"help", no_argument, nullptr, 'h'},
-        {nullptr, 0, nullptr, 0}};
-
-    while (true) {
-        int option_index = 0;
-        int c, val;
-        c = getopt_long(argc, argv, "csdrphf:n:x:l", long_options, &option_index);
-        if (c < 0)
-            break;
-        switch (c) {
-        case 's':
-            bpmsave = true;
-            break;
-        case 'd':
-            redetect = true;
-            break;
-        case 'h':
-            display_help();
-            return 0;
-        case 'r':
-            clear = true;
-            // do not start GUI, just remove BPMs
-            console = true;
-            break;
-        case 'p':
-            progress = false;
-            break;
-        case 'f':
-            bformat = true;
-            format = QString::fromUtf8(optarg);
-            break;
-        case 'n':
-            val = std::stoi(std::string(optarg));
-            Track::setMinBPM(val);
-#ifdef DEBUG
-            cerr << "Min BPM set to " << Track::getMinBPM() << endl;
-#endif
-            break;
-        case 'x':
-            val = std::stoi(std::string(optarg));
-            Track::setMaxBPM(val);
-#ifdef DEBUG
-            cerr << "Max BPM set to " << Track::getMaxBPM() << endl;
-#endif
-            break;
-        case 'l':
-            Track::setLimit(true);
-            break;
-#ifndef NO_GUI
-        case 'c':
-            console = true;
-            break;
-#endif
-        case '?':
-        default:
-            display_help();
-            return 0;
-        }
-    }
-
-#ifdef NO_GUI
-    console = true;
-    if (argc - optind < 1)
+    QApplication app(argc, argv);
 #else
-    QStringList filelist;
-    if (console && argc - optind < 1)
+    QCoreApplication app(argc, argv);
 #endif
-    { // no files passed
-        display_help();
-        return 0;
-    }
 
-    for (int idx = optind; idx < argc; idx++) {
-        if (console) {
-            if (optind != argc - 1)
-                qDebug() << "[" << idx + 1 - optind << "/" << argc - optind << "] " << argv[idx];
-            TrackProxy track(QString::fromUtf8(argv[idx]));
-            if (!clear) {
-                track.enableConsoleProgress(progress);
-                track.setRedetect(redetect);
-                if (bformat)
-                    track.setFormat(format);
+    QCoreApplication::setApplicationName(QStringLiteral("bpmdetect"));
+    QCoreApplication::setApplicationVersion(QStringLiteral("0.7.2"));
+
+    QCommandLineParser parser;
+    parser.setApplicationDescription(
+        QStringLiteral("BPM Detect - automatic BPM detection utility"));
+
+    QCommandLineOption consoleOpt({QStringLiteral("c"), QStringLiteral("console")},
+                                  QStringLiteral("Run in console mode."));
+#ifdef NO_GUI
+    consoleOpt.setFlags(QCommandLineOption::HiddenFromHelp);
+#endif
+    QCommandLineOption saveOpt({QStringLiteral("s"), QStringLiteral("save")},
+                               QCoreApplication::translate("main", "Save BPMs to tags."));
+    QCommandLineOption detectOpt({QStringLiteral("d"), QStringLiteral("detect")},
+                                 QCoreApplication::translate("main", "Redetect BPMs."));
+    QCommandLineOption removeOpt(
+        {QStringLiteral("r"), QStringLiteral("remove")},
+        QCoreApplication::translate("main", "Remove BPM tags and do not perform detection."));
+    QCommandLineOption noprogressOpt(
+        {QStringLiteral("p"), QStringLiteral("no-progress")},
+        QCoreApplication::translate("main", "Disable progress display."));
+    QCommandLineOption minOpt({QStringLiteral("n"), QStringLiteral("min")},
+                              QCoreApplication::translate("main", "Minimum BPM."),
+                              QStringLiteral("value"));
+    QCommandLineOption maxOpt({QStringLiteral("x"), QStringLiteral("max")},
+                              QCoreApplication::translate("main", "Maximum BPM."),
+                              QStringLiteral("value"));
+    QCommandLineOption limitOpt(
+        {QStringLiteral("l"), QStringLiteral("limit")},
+        QCoreApplication::translate("main", "Do not allow a BPM above the range."));
+    QCommandLineOption formatOpt({QStringLiteral("f"), QStringLiteral("format")},
+                                 QCoreApplication::translate("main", "Set BPM format."),
+                                 QStringLiteral("format"),
+                                 QStringLiteral("0.00"));
+
+    parser.addOption(consoleOpt);
+    parser.addOption(detectOpt);
+    parser.addOption(formatOpt);
+    parser.addOption(limitOpt);
+    parser.addOption(maxOpt);
+    parser.addOption(minOpt);
+    parser.addOption(noprogressOpt);
+    parser.addOption(removeOpt);
+    parser.addOption(saveOpt);
+    parser.addHelpOption();
+    parser.addVersionOption();
+    parser.addPositionalArgument(QStringLiteral("files"),
+                                 QCoreApplication::translate("main", "Files to process."));
+    parser.process(app);
+#ifndef NO_GUI
+    isConsoleMode = parser.isSet(consoleOpt);
+#endif
+
+    if (parser.isSet(minOpt)) {
+        Track::setMinBPM(parser.value(minOpt).toDouble());
+        qDebug() << "Min BPM set to" << Track::getMinBPM();
+    }
+    if (parser.isSet(maxOpt)) {
+        Track::setMaxBPM(parser.value(maxOpt).toDouble());
+        qDebug() << "Max BPM set to" << Track::getMaxBPM();
+    }
+    Track::setLimit(parser.isSet(limitOpt));
+    auto files = parser.positionalArguments();
+
+    if (isConsoleMode) {
+        if (files.isEmpty()) {
+            parser.showHelp(0);
+        }
+        for (const auto &file : files) {
+            TrackProxy track(file);
+            if (!parser.isSet(removeOpt)) {
+                track.enableConsoleProgress(!parser.isSet(noprogressOpt));
+                track.setRedetect(parser.isSet(detectOpt));
+                track.setFormat(parser.value(formatOpt));
                 track.detectBPM();
-                if (bpmsave)
-                    track.saveBPM();
                 track.printBPM();
+                if (parser.isSet(saveOpt)) {
+                    track.saveBPM();
+                }
             } else {
                 track.clearBPM();
             }
-        } else {
-#ifndef NO_GUI
-            filelist += QString::fromUtf8(argv[idx]);
-#endif // NO_GUI
         }
+        return 0;
     }
 
 #ifndef NO_GUI
-    if (!console) {
-        QApplication app(argc, argv);
-        app.setStyle(QStringLiteral("plastique"));
-        DlgBPMDetect *mainWin = new DlgBPMDetect();
-        mainWin->show();
-        mainWin->slotAddFiles(filelist);
+    if (!isConsoleMode) {
+        DlgBPMDetect mainWin;
+        mainWin.show();
+        mainWin.slotAddFiles(files);
         app.exec();
-        delete mainWin;
-        mainWin = nullptr;
     }
-#endif // NO_GUI
+#endif
     return 0;
 }
