239429e1579e07c868b915bd9561fc99370c5847
Andrew Udvare <audvare@gmail.com>
2025-08-25 06:46:01 -0400

    docs: add

--- src/debug.h
+++ src/debug.h
@@ -1,4 +1,5 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 #include <QLoggingCategory>
 
+/** Primary logging category. */
 Q_DECLARE_LOGGING_CATEGORY(gLogBpmDetect)
--- src/track/constants.h
+++ src/track/constants.h
@@ -1,4 +1,6 @@
 #pragma once
 
+/** Number of channels used for BPM detection. */
 #define DETECTION_CHANNELS 2
+/** Sample rate used for BPM detection (all files will be resampled to this rate). */
 #define DETECTION_SAMPLE_RATE 48000
