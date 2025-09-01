0c08cf312c5aa2bfe9e60414adb5889e72350131
Andrew Udvare <audvare@gmail.com>
2025-09-01 18:36:01 -0400

    ffmpegutils: use C++-friendly version of av_err2str

--- src/ffmpegutils.cpp
+++ src/ffmpegutils.cpp
@@ -10,6 +10,17 @@ extern "C" {
 #include "debug.h"
 #include "ffmpegutils.h"
 
+// https://github.com/joncampbell123/composite-video-simulator/issues/5#issuecomment-611885908
+#ifdef av_err2str
+#undef av_err2str
+#include <string>
+av_always_inline std::string av_err2string(int errnum) {
+    char str[AV_ERROR_MAX_STRING_SIZE];
+    return av_make_error_string(str, AV_ERROR_MAX_STRING_SIZE, errnum);
+}
+#define av_err2str(err) av_err2string(err).c_str()
+#endif // av_err2str
+
 bool isDecodableFile(const QString &fileName) {
     AVFormatContext *fmt_ctx = nullptr;
     int ret;
